30591693cf84a4f8f237a92503cc72fa34c48b0c
Douglas Shinzato <shinzato.douglas@gmail.com>
2024-04-18 00:13:29 -0400

    add deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "http://douglasshinzato.github.io",
+  base: "/shinzatodev",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
